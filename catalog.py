9058aaceb40ee532752c4972893057cb3437e49e
ehperminute <87844955+ehperminute@users.noreply.github.com>
2026-07-03 13:20:20 -0600

    Update catalog.py

--- catalog.py
+++ catalog.py
@@ -1,7 +1,10 @@
 products = [
-  ("Monitor AOC 27\" QHD ", "Didplays", 250),
-  ("Ryzen 5 7500f", "CPUs", 280),
-  ("RAM 16gb x 2 DDR5 kit 6400 MTs GSkill", "RAM", 600),
-  ("Core ultra 5 250K", "CPUSls", 350),
-  ("RTX 5070ti Nvidia", "GPUs", 750)
+    ("Monitor AOC 27\" QHD", "Displays", 250),
+    ("Ryzen 5 7500F", "CPUs", 280),
+    ("Core Ultra 5 250K", "CPUs", 350),
+    ("RTX 5070 Ti", "GPUs", 750),
+    ("32GB DDR5 Kit", "RAM", 600),
+    ("USB-C Cable 1m", "Accessories", 10),
+    ("Mouse Pad XL", "Accessories", 18),
+    ("Wireless Mouse", "Peripherals", 35),
 ]
