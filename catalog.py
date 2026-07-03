86a75d64d5348efd9e7a14b720989043e47d3e79
ehperminute <87844955+ehperminute@users.noreply.github.com>
2026-07-03 13:17:44 -0600

    Create catalog.py

---
+++ catalog.py
@@ -0,0 +1,7 @@
+items = [
+  ("Monitor AOC 27\" QHD ", "Didplays", 250),
+  ("Ryzen 5 7500f", "CPUs", 280),
+  ("RAM 16gb x 2 DDR5 kit 6400 MTs GSkill", "RAM", 600),
+  ("Core ultra 5 250K", "CPUS", 350),
+  ("RTX 5070ti Nvidia", "GPUs", 750)
+]
