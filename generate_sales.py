e5b019a8a1dfc080928f5039179c06fafd0d4529
ehperminute <87844955+ehperminute@users.noreply.github.com>
2026-07-04 17:45:45 -0600

    Update generate_sales.py

--- generate_sales.py
+++ generate_sales.py
@@ -1,30 +1,28 @@
 import random
-import datetime 
-from generate_customers import N_CUSTOMERS
+import datetime
+from generate_customers import N_CUSTOMERS, gen_customers
 from catalog import products
 
-
-
 N_SALES = 10000
-
-
-
-end = datetime.date(2025, 12, 31)
-delta = end - start
-
+END_DATE = datetime.date(2025, 12, 31)
 
 def gen_sales():
     sales = []
     customers = gen_customers()
+
     for sale_id in range(1, N_SALES + 1):
-        
         customer_id = random.randint(1, N_CUSTOMERS)
-        start = customers[customer_id - 1][3]
+        customer = customers[customer_id - 1]
+        registration_date = customer[3]
+
         product_id = random.randint(1, len(products))
 
+        delta = END_DATE - registration_date
         random_days = random.randrange(delta.days + 1)
-        sale_date = start + datetime.timedelta(days=random_days)
+        sale_date = registration_date + datetime.timedelta(days=random_days)
+
         quantity = random.randint(1, 5)
+
         sales.append((sale_id, customer_id, product_id, quantity, sale_date))
 
     print(sales[:10])
