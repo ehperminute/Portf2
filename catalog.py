c4136c2ca21554ebe0a9837e48915a64af6a3908
ehperminute <87844955+ehperminute@users.noreply.github.com>
2026-07-03 20:13:29 -0600

    Update catalog.py

--- catalog.py
+++ catalog.py
@@ -8,15 +8,15 @@ products = [
     ("Mouse Pad XL", "Accessories", 18),
     ("Wireless Mouse", "Peripherals", 35),
     ("Controller GameSir G7 pro", "Peripherals", 80),
-    ("Mac Mini m4 256gb 16gb RAM", 800),
+    ("Mac Mini m4 256gb 16gb RAM", "Mini PCs", 800),
     ("USB-C USB-A Cable 1m", "Accessories", 10),
     ("Charger Ugreen 30Watt", "Accessories", 30),
     ("Radeon RX 9060 XT Gigabyte 16gb", "GPUs", 600),
 
-    ("SSD ADATA 500GB SATA3", "Storage", 80),
-    ("SSD Fikwot 1tb m2 PCIe4x4", "Storage", "150"),
-    ("SSD Somnambulist 512 GB SATA3", "Storage", 90),
-    ("SSD KingDian 480GB SATA3", "Storage", 80),
+    ("SSD ADATA 500gb SATA3", "Storage", 80),
+    ("SSD Fikwot 1tb m2 PCIe4x4", "Storage", 150),
+    ("SSD Somnambulist 512gb SATA3", "Storage", 90),
+    ("SSD KingDian 480gb SATA3", "Storage", 80),
     ("Charger Tockii 60Watt", "Accessories", 50),
     ("Ryzen 7 7800X3D", "CPUs", 340)
  ]
