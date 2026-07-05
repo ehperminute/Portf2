97431143220c143d31972c3e39f7a7d54c8895aa
ehperminute <87844955+ehperminute@users.noreply.github.com>
2026-07-05 07:44:03 -0600

    Create init_db.py

---
+++ init_db.py
@@ -0,0 +1,30 @@
+from database import get_connection
+
+conn = get_connection()
+cursor = connection.cursor()
+cursor.execute("""
+CREATE TABLE IF NOT EXISTS sales (
+    id INTEGER PRIMARY KEY,
+    customer_id INTEGER NOT NULL REFERENCES customers(id),
+    product_id INTEGER NOT NULL REFERENCES products(id),
+    quantity INTEGER NOT NULL,
+    sale_date DATE NOT NULL
+);""")
+
+cursor.execute("""
+CREATE TABLE IF NOT EXISTS customers (
+    id INTEGER PRIMARY KEY,
+    country TEXT NOT NULL,
+    segment TEXT NOT NULL,
+    registration_date DATE NOT NULL
+);
+""")
+
+cursor.execute("""
+CREATE TABLE IF NOT EXISTS products (
+    id SERIAL PRIMARY KEY,
+    name TEXT NOT NULL,
+    category TEXT NOT NULL,
+    price NUMERIC NOT NULL
+);
+""")
