25900d53e84fa11849649d8ecf5126826f925dd1
ehperminute <87844955+ehperminute@users.noreply.github.com>
2026-07-05 13:53:40 -0600

    Update init_db.py

--- init_db.py
+++ init_db.py
@@ -3,13 +3,14 @@ from database import get_connection
 conn = get_connection()
 cursor = conn.cursor()
 cursor.execute("""
-CREATE TABLE IF NOT EXISTS sales (
-    id INTEGER PRIMARY KEY,
-    customer_id INTEGER NOT NULL REFERENCES customers(id),
-    product_id INTEGER NOT NULL REFERENCES products(id),
-    quantity INTEGER NOT NULL,
-    sale_date DATE NOT NULL
-);""")
+cursor.execute("""
+CREATE TABLE IF NOT EXISTS products (
+    id SERIAL PRIMARY KEY,
+    name TEXT NOT NULL,
+    category TEXT NOT NULL,
+    price NUMERIC NOT NULL
+);
+""")
 
 cursor.execute("""
 CREATE TABLE IF NOT EXISTS customers (
@@ -20,14 +21,17 @@ CREATE TABLE IF NOT EXISTS customers (
 );
 """)
 
-cursor.execute("""
-CREATE TABLE IF NOT EXISTS products (
-    id SERIAL PRIMARY KEY,
-    name TEXT NOT NULL,
-    category TEXT NOT NULL,
-    price NUMERIC NOT NULL
-);
-""")
+CREATE TABLE IF NOT EXISTS sales (
+    id INTEGER PRIMARY KEY,
+    customer_id INTEGER NOT NULL REFERENCES customers(id),
+    product_id INTEGER NOT NULL REFERENCES products(id),
+    quantity INTEGER NOT NULL,
+    sale_date DATE NOT NULL
+);""")
+
+
+
+
 
 conn.commit()
 conn.close()
