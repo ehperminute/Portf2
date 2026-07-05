9208bbb66f46d5214d5b5e91127e2d0560caca43
ehperminute <87844955+ehperminute@users.noreply.github.com>
2026-07-04 19:10:00 -0600

    Create export_csv.py

---
+++ export_csv.py
@@ -0,0 +1,13 @@
+import csv
+from generate_customers import gen_customers
+from generate_sales import gen_sales
+from products import products
+
+
+def export(filename, data):
+    with open(filename, "w", encoding="utf8") as out_f:
+        writer = csv.writer(out_f)
+        writer.writerows(data)
+
+
+for data, filename in ()
