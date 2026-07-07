4b230f59419fb19e4dc37059ab91b60410c37c0f
ehperminute <87844955+ehperminute@users.noreply.github.com>
2026-07-06 20:57:24 -0600

    Update init_db.py

--- init_db.py
+++ init_db.py
@@ -5,29 +5,31 @@ cursor = conn.cursor()
 cursor.execute("""
 
 CREATE TABLE IF NOT EXISTS products (
-    id SERIAL PRIMARY KEY,
+    id INTEGER PRIMARY KEY,
     name TEXT NOT NULL,
     category TEXT NOT NULL,
-    price NUMERIC NOT NULL
+    price NUMERIC NOT NULL CHECK (price > 0)
 );
 """)
-
+               
 cursor.execute("""
 CREATE TABLE IF NOT EXISTS customers (
     id INTEGER PRIMARY KEY,
     country TEXT NOT NULL,
-    segment TEXT NOT NULL,
+    segment TEXT NOT NULL CHECK (segment IN ('Consumer', 'Small Business', 'Enterprise')),
     registration_date DATE NOT NULL
 );
 """)
+
 cursor.execute("""
 CREATE TABLE IF NOT EXISTS sales (
     id INTEGER PRIMARY KEY,
     customer_id INTEGER NOT NULL REFERENCES customers(id),
     product_id INTEGER NOT NULL REFERENCES products(id),
-    quantity INTEGER NOT NULL,
+    quantity INTEGER NOT NULL CHECK (quantity > 0),
     sale_date DATE NOT NULL
-);""")
+);
+""")
 
 
 
